23f1ff4d981e20d5e5aa125ea4e3281110dab90e
jcellomarcano <jcellomarcano@gmail.com>
2019-05-29 11:23:57 -0400

    First Commit

---
+++ respues_app_redes/android/app/src/main/kotlin/site/jcellomarcano/respues_app_redes/MainActivity.kt
@@ -0,0 +1,13 @@
+package site.jcellomarcano.respues_app_redes
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
